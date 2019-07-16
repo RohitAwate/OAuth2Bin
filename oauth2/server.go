35e87277e41cdc9565b3fb6871529fb196497274
Rohit Awate <rohitawate121@gmail.com>
2019-07-16 12:26:07 +0530

    Add 404 page and routing logic

--- oauth2/server.go
+++ oauth2/server.go
@@ -35,27 +35,63 @@ func (s *OA2Server) Start() {
 	public := http.FileServer(http.Dir("public/"))
 	http.Handle("/public/", http.StripPrefix("/public/", public))
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles(
-			"public/templates/index.html",
-			"public/templates/base.html",
-			"public/templates/cards.html",
-		)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		err = tmpl.ExecuteTemplate(w, "home", s.Config)
-		if err != nil {
-			log.Fatal(err)
-		}
-	})
-
-	http.HandleFunc("/authorize", s.Limiter.Handle(handleAuth))
-	http.HandleFunc("/response", s.Limiter.Handle(handleResponse))
-	http.HandleFunc("/token", s.Limiter.Handle(handleToken))
-	http.HandleFunc("/echo", s.Limiter.Handle(handleEcho))
+	s.route("/", s.handleHome)
+	s.route("/authorize", handleAuth)
+	s.route("/response", handleResponse)
+	s.route("/token", handleToken)
+	s.route("/echo", handleEcho)
 
 	log.Printf("OAuth 2.0 Server has started on port %s.\n", s.Port)
-	http.ListenAndServe(":"+s.Port, nil)
+	http.ListenAndServe("0.0.0.0:"+s.Port, nil)
+}
+
+// Registers a callback for the specified URL pattern.
+// The callback first examines if request route matches the pattern.
+// If not, the 404 logic is triggered. This is the purpose behind the existence
+// of this method.
+//
+// However, if the paths match, the IP limiter is triggered which returns another callback.
+// This callback is called by passing it the RequestWriter and the Request pointer.
+func (s *OA2Server) route(pattern string, handler http.HandlerFunc) {
+	http.HandleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != pattern {
+			handleNotFound(w, r)
+			return
+		}
+
+		s.Limiter.Handle(handler)(w, r)
+	})
+}
+
+// Serves the home page
+func (s *OA2Server) handleHome(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles(
+		"public/templates/index.html",
+		"public/templates/base.html",
+		"public/templates/cards.html",
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tmpl.ExecuteTemplate(w, "home", s.Config)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Serves the 404 page
+func handleNotFound(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles(
+		"public/templates/404.html",
+		"public/templates/base.html",
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tmpl.ExecuteTemplate(w, "404", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
