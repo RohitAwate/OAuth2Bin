dca073c1132de80dda87a354ad314a0df6eca1de
Rohit Awate <rohitawate121@gmail.com>
2019-07-18 20:22:45 +0530

    Fix issue where fallback copy mechanism was unreachable

--- public/static/index.js
+++ public/static/index.js
@@ -6,28 +6,28 @@ window.onload = e => {
     const copyTargets = document.querySelectorAll('.copy');
     for (target of copyTargets) {
         target.addEventListener('click', e => {
-            // Using the new Clipboard API 
-            const promise = navigator.clipboard.writeText(e.target.innerText);
-            
-            /*
-                The Clipboard API is not fully supported yet and hence,
-                we use this junk as a fallback mechanism. (also for IE)
-                Refer: https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/writeText#Browser_compatibility
+            // Using the new Clipboard API
+            if (navigator.clipboard) {
+                navigator.clipboard.writeText(e.target.innerText);
+            } else {
+                /*
+                    The Clipboard API is not fully supported yet and hence,
+                    we use this junk as a fallback mechanism. (also for IE)
+                    Refer: https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/writeText#Browser_compatibility
 
-                Steps:
-                - first create an empty text area
-                - add the text from the copy target to it
-                - append the text area to the DOM tree
-                - focus and select it
-                - copy the selected text
-                - unselect everything
-                - remove the field from the DOM tree
+                    Steps:
+                    - first create an empty text area
+                    - add the text from the copy target to it
+                    - append the text area to the DOM tree
+                    - focus and select it
+                    - copy the selected text
+                    - unselect everything
+                    - remove the field from the DOM tree
 
-                Known issue:
-                On appending the text area to the DOM, the added height causes the
-                page to scroll down.
-             */
-            promise.catch(err => {
+                    Known issue:
+                    On appending the text area to the DOM, the added height causes the
+                    page to scroll down.
+                */
                 const field = document.createElement('textarea');
                 field.value = e.target.innerHTML;
                 document.body.appendChild(field);
@@ -36,7 +36,7 @@ window.onload = e => {
                 document.execCommand('copy');
                 window.getSelection().removeAllRanges();
                 field.remove();
-            });
+            }
 
             showAlert("Copied to clipboard!", 2000);
         });
